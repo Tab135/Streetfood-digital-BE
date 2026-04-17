70a3e61e3afae815a823d06018d79799cb111c0f
Tab135 <70458035+Tab135@users.noreply.github.com>
2026-04-17 11:54:31 +0700

    Add IsActive to Badge (EF migration)

---
+++ DAL/Migrations/20260417040808_AddACtivetobadge.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddACtivetobadge : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Tastes",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "FeedbackTags",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "DietaryPreferences",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Categories",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Badges",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "DietaryPreferences",
+                keyColumn: "dietaryPreferenceId",
+                keyValue: 1,
+                column: "IsActive",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "DietaryPreferences",
+                keyColumn: "dietaryPreferenceId",
+                keyValue: 2,
+                column: "IsActive",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "DietaryPreferences",
+                keyColumn: "dietaryPreferenceId",
+                keyValue: 3,
+                column: "IsActive",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "DietaryPreferences",
+                keyColumn: "dietaryPreferenceId",
+                keyValue: 4,
+                column: "IsActive",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "DietaryPreferences",
+                keyColumn: "dietaryPreferenceId",
+                keyValue: 5,
+                column: "IsActive",
+                value: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Tastes");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "FeedbackTags");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "DietaryPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Badges");
+        }
+    }
+}
